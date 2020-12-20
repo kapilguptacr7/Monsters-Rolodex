e86527dbf7ba2e9a31cedc2a60ad4a4efa9dbb0d
kapilguptacr7 <kapilguptacr7@gmail.com>
2020-12-20 21:15:41 +0530

    adding files to github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import { getSuggestedQuery } from "@testing-library/react";
+import React from "react";
+import "./card.styles.css";
+
+export const Card = (props) => (
+  <div className="card-container">
+    <img
+      alt="monster"
+      src={`https://robohash.org/${props.monster.id}?set=set2`}
+    />
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
+);
